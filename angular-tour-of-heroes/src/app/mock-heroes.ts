50f02fd0c99603ec28753efbbaf6cf9446f8b021
Takashi <ygortn14@gmail.com>
2021-02-01 15:29:06 -0400

    Aula 2 - Exibição de listas, eventos de cliques, estruturas de repetição e seleção

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Takashi'},
+    {id: 12, name: 'Flash'},
+    {id: 13, name: 'Batman'},
+    {id: 14, name: 'Homem de Ferro'},
+    {id: 15, name: 'Naruto'},
+    {id: 16, name: 'Goku'},
+    {id: 17, name: 'Capitã Marvel'},
+    {id: 18, name: 'Doutor Estranho'},
+    {id: 19, name: 'Feiticeira Escarlate'},
+    {id: 20, name: 'Hulk'}
+];
